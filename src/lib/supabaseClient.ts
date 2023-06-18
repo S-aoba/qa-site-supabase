b2199d384a683dda2d3d75b3d44f9ce3c19420f0
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-06-18 19:15:26 +0900

    ➕ 🎨 add:auth-helper

--- src/lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-import type { Database } from './database.types'
-
-export const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string
-)
