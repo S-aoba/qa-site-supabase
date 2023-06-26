bd5d71d449b1e94254e5cff792d841fb94cc846c
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-06-26 17:18:12 +0900

    💄 add: answer edit delete icon

--- src/components/answer.tsx
+++ src/components/answer.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { IconEdit, IconTrash } from '@tabler/icons-react'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 
@@ -22,7 +23,7 @@ export const Answer = async ({ userId }: { userId: string | undefined }) => {
       <div className='p-2 text-2xl font-semibold'>Answer</div>
       <div className='space-y-4 py-4'>
         {answers?.map((answer) => {
-          return <Description key={answer.id} answer={answer} />
+          return <Description key={answer.id} answer={answer} userId={userId} />
         })}
         {userId && <AnswerForm userId={userId} />}
       </div>
@@ -30,30 +31,38 @@ export const Answer = async ({ userId }: { userId: string | undefined }) => {
   )
 }
 
-const Description = async ({ answer }: { answer: AnswerType }) => {
+const Description = async ({ answer, userId }: { answer: AnswerType; userId: string | undefined }) => {
   const supabase = createClientComponentClient<Database>()
   const { data: profile } = await supabase.from('profiles').select('*').eq('id', answer?.user_id).single()
 
   return (
     <div className='rounded-lg border border-solid border-slate-300 pb-5'>
       <div className='rounded-t-lg border-b border-l-0 border-r-0 border-t-0 border-solid border-slate-300 bg-[#f6f8fa] px-2'>
-        <div className='flex items-center space-x-2'>
-          <div className='relative h-6 w-6'>
-            <Image
-              src={profile && profile.avatar_url ? profile.avatar_url : '/default.png'}
-              className='rounded-full object-cover'
-              alt='avatar'
-              fill
-              sizes='auto'
-              priority
-            />
+        <div className='flex justify-between'>
+          <div className='flex items-center space-x-2'>
+            <div className='relative h-6 w-6'>
+              <Image
+                src={profile && profile.avatar_url ? profile.avatar_url : '/default.png'}
+                className='rounded-full object-cover'
+                alt='avatar'
+                fill
+                sizes='auto'
+                priority
+              />
+            </div>
+            <div>
+              <p className='text-sm'>{profile?.username}</p>
+            </div>
+            <div>
+              <span className='text-sm'>投稿日: {answer.created_at.slice(0, 10)}</span>
+            </div>
           </div>
-          <div>
-            <p className='text-sm'>{profile?.username}</p>
-          </div>
-          <div>
-            <span className='text-sm'>投稿日: {answer.created_at.slice(0, 10)}</span>
-          </div>
+          {userId === answer.user_id && (
+            <div className='flex items-center space-x-2'>
+              <IconEdit className='text-slate-500 hover:cursor-pointer hover:text-slate-700' />
+              <IconTrash className='text-slate-500 hover:cursor-pointer hover:text-slate-700' />
+            </div>
+          )}
         </div>
       </div>
       {answer && <div className='break-words p-3' dangerouslySetInnerHTML={{ __html: answer.description }} />}
