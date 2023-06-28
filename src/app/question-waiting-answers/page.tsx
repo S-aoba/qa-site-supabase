ed3953a1fc9aa16a6e7bdcac9094a65aba32875c
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-06-28 13:33:47 +0900

    👔 update:  descending order

--- src/app/question-waiting-answers/page.tsx
+++ src/app/question-waiting-answers/page.tsx
@@ -6,7 +6,11 @@ import type { Database } from '@/lib/database.types'
 
 const QuestionWaitingAnswers = async () => {
   const supabase = createServerComponentClient<Database>({ cookies })
-  const { data: questions } = await supabase.from('questions').select('*').eq('is_answered', false)
+  const { data: questions } = await supabase
+    .from('questions')
+    .select('*')
+    .eq('is_answered', false)
+    .order('updated_at', { ascending: false })
 
   return (
     <main className='flex flex-col justify-center space-y-4'>
