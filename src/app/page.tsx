177a6f9beada9670c3e6f5ada9723e4b97ecf33a
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-06-28 13:32:06 +0900

    👔  update: questions descending order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import type { Database } from '@/lib/database.types'
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies })
-  const { data: questions } = await supabase.from('questions').select('*')
+  const { data: questions } = await supabase.from('questions').select('*').order('updated_at', { ascending: false })
 
   return (
     <main className='flex flex-col justify-center space-y-4'>
