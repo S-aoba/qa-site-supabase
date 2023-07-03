ceb12f0d4f8234b8a77c57104589675183e1b847
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-03 22:16:52 +0900

    ✨ add:update comment

---
+++ src/components/comment-update-form.tsx
@@ -0,0 +1,68 @@
+'use client'
+import { Button } from '@mantine/core'
+import { RichTextEditor } from '@mantine/tiptap'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useAtomValue } from 'jotai'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+import Loading from '@/app/loading'
+import { useContentEditor } from '@/common/hooks/useContentEditor'
+import type { Database } from '@/lib/database.types'
+import { editedCommentAtom } from '@/store/comment-atom'
+
+export const CommentUpdateForm = ({ commentId }: { commentId: string }) => {
+  const supabase = createClientComponentClient<Database>()
+
+  const [isLoading, setIsLoading] = useState(false)
+  const { commentEditor } = useContentEditor({ type: 'comment' })
+  const comment = useAtomValue(editedCommentAtom)
+  const [message, setMessage] = useState('')
+  const router = useRouter()
+
+  const handleOnSubmit = async () => {
+    setIsLoading(true)
+
+    try {
+      const { error: updateCommentError } = await supabase
+        .from('comments')
+        .update({
+          content: comment,
+        })
+        .eq('id', commentId)
+      if (updateCommentError) {
+        setMessage('予期せぬエラーが発生しました。' + updateCommentError.message)
+        return
+      }
+      router.refresh()
+    } catch (error) {
+      setMessage('エラーが発生しました。' + error)
+      return
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <>
+      <form className='p-2' onSubmit={handleOnSubmit}>
+        <RichTextEditor
+          editor={commentEditor}
+          className=' min-h-[400px] w-full rounded-md border border-solid border-slate-300 shadow'
+        >
+          <RichTextEditor.Content />
+        </RichTextEditor>
+        <div className='flex w-full justify-end p-3'>
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <Button type='submit' className='bg-slate-500 hover:transform-none hover:bg-slate-600'>
+              コメントを更新する
+            </Button>
+          )}
+        </div>
+      </form>
+      {message && <div className='my-5 text-center text-sm text-red-500'>{message}</div>}
+    </>
+  )
+}
--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -1,10 +1,26 @@
 'use client'
 import { IconEdit, IconTrash } from '@tabler/icons-react'
+import { useSetAtom } from 'jotai'
 import Image from 'next/image'
+import { useState } from 'react'
 
 import type { CommentType } from '@/common/types'
+import { editedCommentAtom } from '@/store/comment-atom'
+
+import { CommentUpdateForm } from './comment-update-form'
 
 export const Comment = ({ comment, userId }: { comment: CommentType; userId: string | undefined }) => {
+  const [isEditMode, setIsEditMode] = useState(false)
+  const setComment = useSetAtom(editedCommentAtom)
+
+  const handleSetComment = () => {
+    if (!isEditMode) {
+      setComment(comment.content)
+      setIsEditMode(true)
+      return
+    }
+    setIsEditMode(false)
+  }
   return (
     <div className='border-b border-l-0 border-r-0 border-t-0 border-solid border-slate-300'>
       <div className='flex justify-between space-x-2 bg-[#d3e0ec] p-2'>
@@ -23,12 +39,16 @@ export const Comment = ({ comment, userId }: { comment: CommentType; userId: str
         </div>
         {userId && userId === comment.user_id ? (
           <div className='flex space-x-2'>
-            <IconEdit className='text-slate-500 hover:cursor-pointer hover:text-slate-700' />
+            <IconEdit className='text-slate-500 hover:cursor-pointer hover:text-slate-700' onClick={handleSetComment} />
             <IconTrash className='text-slate-500 hover:cursor-pointer hover:text-slate-700' />
           </div>
         ) : null}
       </div>
-      {comment && <div className='break-words px-2' dangerouslySetInnerHTML={{ __html: comment.content }} />}
+      {isEditMode ? (
+        <CommentUpdateForm commentId={comment.id} />
+      ) : (
+        comment && <div className='break-words px-2' dangerouslySetInnerHTML={{ __html: comment.content }} />
+      )}
     </div>
   )
 }
