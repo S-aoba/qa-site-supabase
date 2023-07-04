5865152a08f2eace35159537fd0ef66794b75efc
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-04 12:12:45 +0900

    💄 fix: border top

--- src/components/comment-create-form.tsx
+++ src/components/comment-create-form.tsx
@@ -63,7 +63,7 @@ export const CommentCreateForm = ({ answer, userId }: { answer: AnswerType; user
   return (
     <>
       {userId && (
-        <div className='p-2'>
+        <div className='p-2 border-b-0 border-l-0 border-t border-r-0 border-solid border-slate-300'>
           <div className='flex items-center space-x-2  pt-2 font-semibold text-slate-400'>
             <div className='relative h-6 w-6'>
               <Image src={avatarUrl} className='rounded-full object-cover' alt='avatar' fill sizes='auto' priority />
