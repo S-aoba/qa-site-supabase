92cb66bde10dcde50e9a54f1fe4c70f8848dd4ab
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-05 23:11:27 +0900

    💄 add: if not question

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,14 @@ export default async function Home() {
   const { data: questions } = await supabase.from('questions').select('*').order('updated_at', { ascending: false })
 
   return (
-    <main className='flex flex-col justify-center space-y-4'>
-      {questions?.map((question) => {
-        return <Card key={question.id} question={question} />
-      })}
+    <main className='flex flex-col items-center justify-center space-y-4'>
+      {questions && questions.length === 0 ? (
+        <div>質問はありません</div>
+      ) : (
+        questions?.map((question) => {
+          return <Card key={question.id} question={question} />
+        })
+      )}
     </main>
   )
 }
