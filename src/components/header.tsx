ef1a261e35370d516f1ade37eb85bdf79958d9b4
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-06 13:33:35 +0900

    fix: login signup word

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -81,14 +81,17 @@ export const Header = ({
               <Link href='/auth/login' className='no-underline'>
                 <Button
                   type='submit'
-                  className='rounded-lg border border-solid border-slate-300 bg-white text-black hover:transform-none hover:bg-white hover:border-black'
+                  className='rounded-lg border border-solid border-slate-300 bg-white text-black hover:transform-none hover:border-black hover:bg-white'
                 >
-                  Log In
+                  ログイン
                 </Button>
               </Link>
               <Link href='/auth/signup' className='no-underline'>
-                <Button type='submit' className='rounded-lg bg-black hover:transform-none hover:opacity-75 hover:bg-black'>
-                  Sign Up
+                <Button
+                  type='submit'
+                  className='rounded-lg bg-black hover:transform-none hover:bg-black hover:opacity-75'
+                >
+                  新規登録
                 </Button>
               </Link>
             </div>
