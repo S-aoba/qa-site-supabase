95560cd4b43dbc1a048926c41041e230dfd9018d
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-06 16:16:13 +0900

    #47 🚚  mv:related to auth files

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -2,7 +2,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-import { ResetPassword } from '@/components/reset-password'
+import { ResetPassword } from '@/components/auth/reset-password'
 import type { Database } from '@/lib/database.types'
 
 // パスワードリセットページ
