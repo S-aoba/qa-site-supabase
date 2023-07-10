a7491348a0d725cada6d377d69c7484e0b1f6a56
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-10 22:38:41 +0900

    🩹 add:question edit page

---
+++ src/app/questions/edit/page.tsx
@@ -0,0 +1,25 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { QuestionEdit } from '@/components/question/question-edit'
+import type { Database } from '@/lib/database.types'
+
+const QuestionEditPage = async () => {
+  const supabase = createServerComponentClient<Database>({
+    cookies,
+  })
+
+  // セッションの取得
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  // 未認証の場合、リダイレクト
+  if (!session) {
+    redirect('/auth/login')
+  }
+  return <QuestionEdit />
+}
+
+export default QuestionEditPage
