d6d3cad5efc5fbdf3e4951b4e9d6074fa4c84796
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-10 23:00:32 +0900

    👔 add:delete comment modal

--- src/components/comment/comment-actions.tsx
+++ src/components/comment/comment-actions.tsx
@@ -1,9 +1,11 @@
+import { Button, Modal } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { IconEdit, IconTrash } from '@tabler/icons-react'
 import type { SetStateAction } from 'jotai'
 import { useSetAtom } from 'jotai'
 import { useRouter } from 'next/navigation'
-import type { Dispatch } from 'react'
+import { type Dispatch, useState } from 'react'
 
 import type { CommentType } from '@/common/types'
 import type { Database } from '@/lib/database.types'
@@ -25,6 +27,12 @@ export const CommentActions = ({
   const router = useRouter()
   const supabase = createClientComponentClient<Database>()
   const setComment = useSetAtom(editedCommentAtom)
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [isDeleteAnswerOpened, { open: handleDeleteQuestionOpen, close: handleDeleteAnswerClose }] =
+    useDisclosure(false)
+
   const handleSetComment = () => {
     if (!isEditMode) {
       setComment(comment.content)
@@ -35,26 +43,57 @@ export const CommentActions = ({
   }
 
   const handleDeleteComment = async () => {
+    setIsLoading(true)
     try {
       const { error: deleteCommentError } = await supabase.from('comments').delete().eq('id', comment.id)
       if (deleteCommentError) {
         setMessage('予期せぬエラーが発生しました。' + deleteCommentError.message)
         return
       }
-      router.refresh()
     } catch (error) {
       setMessage('エラーが発生しました。' + error)
       return
+    } finally {
+      setIsLoading(false)
+      router.refresh()
     }
   }
   return (
     <>
+      <Modal opened={isDeleteAnswerOpened} onClose={handleDeleteAnswerClose} centered withCloseButton={false}>
+        <div className='w-full p-5'>
+          <div className='mb-4 border-b border-l-0 border-r-0 border-t-0 border-solid border-gray-200'>
+            <div className='mb-4 flex flex-col rounded-md bg-gray-100 p-3'>
+              <span>削除してもよろしいですか？</span>
+              <span>この手順は取り消すことはできません。</span>
+            </div>
+          </div>
+          <div className='flex w-full justify-end gap-x-3'>
+            <Button
+              variant='default'
+              type='button'
+              onClick={handleDeleteAnswerClose}
+              className=' hover:transform-none hover:border-black hover:bg-white'
+            >
+              キャンセル
+            </Button>
+            <Button
+              type='button'
+              className='bg-red-500 hover:transform-none hover:bg-red-500 hover:opacity-75'
+              onClick={handleDeleteComment}
+              loading={isLoading}
+            >
+              {isLoading ? '削除中' : '削除'}
+            </Button>
+          </div>
+        </div>
+      </Modal>
       {userId === comment.user_id && (
         <div className='flex space-x-2'>
           <IconEdit className='text-slate-500 hover:cursor-pointer hover:text-slate-700' onClick={handleSetComment} />
           <IconTrash
             className='text-slate-500 hover:cursor-pointer hover:text-slate-700'
-            onClick={handleDeleteComment}
+            onClick={handleDeleteQuestionOpen}
           />
         </div>
       )}
