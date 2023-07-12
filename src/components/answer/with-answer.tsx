b962ba758f0ddb2c54cb224ece7d5391be5c431f
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-12 18:42:21 +0900

    💄 回答数の表示追加

--- src/components/answer/with-answer.tsx
+++ src/components/answer/with-answer.tsx
@@ -15,7 +15,9 @@ export const WithAnswer = ({
 }) => {
   return (
     <>
-      <div className='p-2 text-2xl font-semibold'>回答</div>
+      <div className='p-2 text-2xl font-semibold'>
+        <span className='text-slate-500'>{answers.length}</span>件回答
+      </div>
       <div className='space-y-4 py-4'>
         {answers.map((answer) => {
           return <Answer key={answer.id} answer={answer} profile={profile} userId={userId} />
