349ff155118bcefcce156515082df2794829a4b3
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-13 15:19:47 +0900

    ♻️ refactor: user-info tags

--- src/components/question/question.tsx
+++ src/components/question/question.tsx
@@ -1,12 +1,12 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import Image from 'next/image'
-import Link from 'next/link'
 
 import NotFound from '@/app/not-found'
 import type { Database } from '@/lib/database.types'
 
 import { AnswerList } from '../answer/answer-list'
+import { QuestionTags } from './question-tags'
+import { QuestionUserInfo } from './question-user-info'
 
 export const Question = async ({ userId, question_id }: { userId: string | undefined; question_id: string }) => {
   const supabase = createServerComponentClient<Database>({
@@ -21,7 +21,7 @@ export const Question = async ({ userId, question_id }: { userId: string | undef
     .eq('id', question_id)
     .single()
 
-  if (error) return <NotFound />
+  if (error || question.profiles === null) return <NotFound />
 
   const answers = question.answers
   const profile = question.profiles
@@ -30,53 +30,20 @@ export const Question = async ({ userId, question_id }: { userId: string | undef
     <>
       <div className='p-2'>
         <div className='text-center'>
-          <h1>{question?.title}</h1>
+          <h1>{question.title}</h1>
         </div>
         <div className='rounded-lg border border-solid border-slate-300 pb-5'>
           <div className='rounded-t-lg border-b border-l-0 border-r-0 border-t-0 border-solid border-slate-300 bg-[#f6f8fa] px-2'>
             <div className='flex justify-between'>
-              <div className='flex items-center space-x-2'>
-                <div className='relative h-6 w-6'>
-                  <Image
-                    src={profile && profile.avatar_url ? profile.avatar_url : '/default.png'}
-                    className='rounded-full object-cover'
-                    alt='avatar'
-                    fill
-                    sizes='auto'
-                    priority
-                  />
-                </div>
-                <p className='text-sm'>{profile?.username}</p>
-                <span className='text-sm'>投稿日: {question?.created_at.slice(0, 10)}</span>
-                <span className='line-clamp-1 w-fit max-w-[500px] rounded-lg bg-slate-500 px-2 py-1 text-sm leading-5 text-stone-50'>
-                  {question?.coding_problem}
-                </span>
-              </div>
+              <QuestionUserInfo
+                created_at={question.created_at}
+                avatar_url={profile.avatar_url}
+                username={profile.username}
+                coding_problem={question.coding_problem}
+              />
               {/* <QuestionActions userId={userId} question={question} setMessage={setMessage} /> */}
             </div>
-            <div className='flex space-x-3 py-2 text-sm'>
-              {question?.tags.map((tag, index) => {
-                return (
-                  <Link
-                    key={index}
-                    href={'/'}
-                    className='flex items-center space-x-2 rounded-xl border border-solid border-slate-400 px-2 py-1 text-black no-underline'
-                  >
-                    <div className='relative h-4 w-4'>
-                      <Image
-                        src={`/lang-icon/${tag}.svg`}
-                        className='rounded-full object-cover'
-                        alt='avatar'
-                        fill
-                        sizes='auto'
-                        priority
-                      />
-                    </div>
-                    <span className='text-slate-600'>{tag}</span>
-                  </Link>
-                )
-              })}
-            </div>
+            <QuestionTags tags={question.tags} />
           </div>
           {question && <div className='break-words p-3' dangerouslySetInnerHTML={{ __html: question.content }} />}
         </div>
