486e53e1e7d07839a7bf95696666d0a93be94132
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-16 22:16:23 +0900

    🩹 関数をuseEffect内に移動

--- src/components/answer/useAnswerFormAlert.ts
+++ src/components/answer/useAnswerFormAlert.ts
@@ -1,27 +1,23 @@
 'use client'
 
 import { useAtomValue } from 'jotai'
-import { useCallback, useEffect } from 'react'
+import { useEffect } from 'react'
 
 import { editedAnswerAtom } from '@/store/answer-atom'
 
 export const useAnswerFormAlert = () => {
   const content = useAtomValue(editedAnswerAtom)
 
-  const handleBeforeUnload = useCallback(
-    (e: BeforeUnloadEvent) => {
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
       if (content === '') return
       e.preventDefault()
       e.returnValue = ''
       return true
-    },
-    [content]
-  )
-
-  useEffect(() => {
+    }
     window.addEventListener('beforeunload', handleBeforeUnload)
     return () => {
       window.removeEventListener('beforeunload', handleBeforeUnload)
     }
-  }, [handleBeforeUnload])
+  }, [content])
 }
