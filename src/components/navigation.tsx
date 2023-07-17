f696f0d3c5a6f76c3db38981ee469c0881221097
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-17 18:46:56 +0900

    💄 change:navigation ui

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -22,19 +22,19 @@ export const Navigation = () => {
   const pathname = usePathname()
 
   return (
-    <div className='flex w-full justify-center border-b border-l-0 border-r-0 border-t-0 border-solid border-slate-200 bg-[#f6f8fa] pb-2'>
+    <div className='flex w-full justify-center border-b border-l-0 border-r-0 border-t-0 border-solid border-slate-200 bg-[#f6f8fa]'>
       <div className=' flex w-full max-w-[800px] space-x-2 px-3 text-sm font-bold'>
         {subNavigation.map((item, index) => {
           return (
-            <Link key={index} href={item.href} className='font-normal text-slate-500 no-underline'>
-              <div
-                className={`${
-                  item.href == pathname && 'bg-slate-700 text-slate-50 hover:text-slate-50'
-                } flex items-center rounded-full px-3 py-2 hover:bg-slate-700 hover:text-white`}
-              >
-                <item.icon className='mr-2 inline-block h-5 w-5' />
-                {item.name}
-              </div>
+            <Link
+              key={index}
+              href={item.href}
+              className={`${
+                item.href == pathname &&
+                'border-b-2 border-l-0 border-r-0 border-t-0 border-solid border-slate-900 text-slate-800 hover:text-slate-800'
+              } flex items-center px-3 py-2 text-slate-500 no-underline hover:text-slate-800`}
+            >
+              {item.name}
             </Link>
           )
         })}
