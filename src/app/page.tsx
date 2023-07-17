2c69346ced27ff8a21326e5c6c02cb196559b185
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-17 14:05:33 +0900

    ♻️ refactor: separate new-question-list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,5 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
+import { NewQuestionList } from '@/components/new-question-list/new-question-list'
 
-import { Card } from '@/components/card'
-import type { Database } from '@/lib/database.types'
-
-import NotFound from './not-found'
-
-export default async function Home() {
-  const supabase = createServerComponentClient<Database>({ cookies })
-  const { data: questions, error } = await supabase
-    .from('questions')
-    .select('*')
-    .order('updated_at', { ascending: false })
-
-  if (error) return <NotFound />
-
-  return (
-    <main className='flex flex-col items-center justify-center space-y-4'>
-      {questions.length === 0 ? (
-        <div>質問はありません</div>
-      ) : (
-        questions.map((question) => {
-          return <Card key={question.id} question={question} />
-        })
-      )}
-    </main>
-  )
+export default function Home() {
+  return <NewQuestionList />
 }
---
+++ src/components/new-question-list/new-question-list.tsx
@@ -0,0 +1,30 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+import NotFound from '@/app/not-found'
+import type { Database } from '@/lib/database.types'
+
+import { Card } from '../card'
+
+export const NewQuestionList = async () => {
+  const supabase = createServerComponentClient<Database>({ cookies })
+  const { data: questions, error } = await supabase
+    .from('questions')
+    .select('*')
+    .order('updated_at', { ascending: false })
+    .limit(10)
+
+  if (error) return <NotFound />
+
+  return (
+    <main className='flex flex-col items-center justify-center space-y-4'>
+      {questions.length === 0 ? (
+        <div>質問はありません</div>
+      ) : (
+        questions.map((question) => {
+          return <Card key={question.id} question={question} />
+        })
+      )}
+    </main>
+  )
+}
