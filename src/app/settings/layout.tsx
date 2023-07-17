dd9dcc7d62c2428d5afaedffd4bea1482a240116
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-17 21:07:19 +0900

    💄 update:setting navigation UI

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -50,24 +50,24 @@ const SettingsLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname()
 
   return (
-    <div className='grid grid-cols-3 gap-3'>
-      <div className='col-span-1 flex flex-col space-y-1 text-sm font-bold'>
+    <div className='grid grid-cols-12 gap-3'>
+      <div className='col-span-2 flex flex-col space-y-1 text-sm font-bold'>
         {subNavigation.map((item, index) => {
           return (
-            <Link href={item.href} key={index} className='no-underline'>
-              <div
-                className={`${
-                  item.href == pathname && 'bg-slate-200 text-slate-500 hover:bg-slate-200 hover:text-slate-500'
-                } flex items-center rounded-full px-3 py-2 text-black hover:bg-slate-100 hover:text-slate-500`}
-              >
-                <item.icon className='mr-2 inline-block h-5 w-5' />
-                {item.name}
-              </div>
+            <Link
+              href={item.href}
+              key={index}
+              className={`${
+                item.href == pathname && 'bg-slate-900 text-white hover:bg-slate-900 hover:text-white'
+              } flex items-center justify-start rounded-full px-3 py-2 text-black no-underline hover:bg-slate-200`}
+            >
+              <item.icon className='mr-2 inline-block h-5 w-5' />
+              {item.name}
             </Link>
           )
         })}
       </div>
-      <div className='col-span-2'>{children}</div>
+      <div className='col-span-10'>{children}</div>
     </div>
   )
 }
