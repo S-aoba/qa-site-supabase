bd491cafcaec316ac2dec4c4e067183f3ad8ccf1
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-07-28 22:48:57 +0900

    💄 fix:ui

--- src/components/header/header-no-session.tsx
+++ src/components/header/header-no-session.tsx
@@ -6,12 +6,12 @@ export const HeaderNoSession = () => {
   return (
     <>
       <Button variant='outline' asChild>
-        <Link href='/auth/login' className='no-underline'>
+        <Link href='/auth/login' className='text-black no-underline'>
           ログイン
         </Link>
       </Button>
       <Button variant='default' asChild>
-        <Link href='/auth/signup' className='no-underline'>
+        <Link href='/auth/signup' className='text-white no-underline'>
           新規登録
         </Link>
       </Button>
--- src/components/question/question-message.tsx
+++ src/components/question/question-message.tsx
@@ -8,11 +8,13 @@ export const QuestionMessage = () => {
   return (
     <div className=' flex flex-col space-y-2 rounded-lg border border-solid border-slate-300 bg-[#f6f8fa] py-10 text-center'>
       <span>あなたも回答してみませんか？</span>
-      <Button type='button' variant='default' asChild>
-        <Link href={'/auth/signup'} className='text-black no-underline'>
-          新規登録
-        </Link>
-      </Button>
+      <div>
+        <Button type='button' variant='default' asChild>
+          <Link href={'/auth/signup'} className='text-black no-underline'>
+            新規登録
+          </Link>
+        </Button>
+      </div>
       <p>
         すでにアカウントを持っている方は<Link href={'/auth/login'}>ログイン</Link>
       </p>
