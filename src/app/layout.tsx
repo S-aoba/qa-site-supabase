d015991a30ea15be246d988b56fab1c5da5f1fbb
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-08-06 14:24:04 +0900

    🔥 rm:mantine provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 
 import { Navigation } from '@/components/navigation'
 import { SupabaseListener } from '@/components/supabase-listener'
-import { Provider } from '@/lib/provider'
 
 export const metadata = {
   title: 'QA app',
@@ -13,11 +12,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='ja'>
       <body className='max-w-screen vsc-initialized flex h-screen flex-col'>
-        <Provider>
-          <SupabaseListener />
-          <Navigation />
-          <main className='container mx-auto max-w-screen-lg p-5'>{children}</main>
-        </Provider>
+        <SupabaseListener />
+        <Navigation />
+        <main className='container mx-auto max-w-screen-lg p-5'>{children}</main>
       </body>
     </html>
   )
