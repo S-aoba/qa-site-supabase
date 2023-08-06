ceb586d7aba4cccbd34ad84daf6ae145ee1350cb
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-08-06 20:50:43 +0900

    🩹 change: favicon Logoの変更

--- src/components/header/header-logo.tsx
+++ src/components/header/header-logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export const HeaderLogo = () => {
   return (
     <Link href={'/'}>
-      <Image src={'/qa-logo.svg'} width={45} height={45} alt='Logo' priority/>
+      <Image src={'/qa-logo.jpg'} width={45} height={45} alt='Logo' priority/>
     </Link>
   )
 }
