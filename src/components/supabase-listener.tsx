a6fd13d7ecf91afa55bb1ce0faa8502dc879f903
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-08-10 11:55:35 +0900

    🚚  fix/rename: subNavigation file

--- src/components/supabase-listener.tsx
+++ src/components/supabase-listener.tsx
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers'
 
 import type { Database } from '@/lib/database.types'
 
-import { SubNavigation } from './navigation/subNavigation'
+import { SubNavigation } from './navigation/sub-navigation'
 
 // 認証状態の監視
 export const SupabaseListener = async () => {
