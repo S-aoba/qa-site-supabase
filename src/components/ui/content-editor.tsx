a2c39d3ca123ac4b4422ff9e04fea1372d947add
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-08-11 20:45:45 +0900

    💄 fix: content-editorの高さを変更

--- src/components/ui/content-editor.tsx
+++ src/components/ui/content-editor.tsx
@@ -4,5 +4,5 @@ import type { Editor } from '@tiptap/react'
 import { EditorContent } from '@tiptap/react'
 
 export const ContentEditor = ({ editor }: { editor: Editor | null }) => {
-  return <EditorContent editor={editor} className=' min-h-[400px] h-min rounded-lg border bg-white p-2' />
+  return <EditorContent editor={editor} className='min-h-[200px] rounded-lg border bg-white p-2' />
 }
