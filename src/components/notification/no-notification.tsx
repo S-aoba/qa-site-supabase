41f4b8bd919de54ab80fa12509a63e3a38314a43
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-08-13 15:54:21 +0900

    💄 #88 sup: notification dark mode

--- src/components/notification/no-notification.tsx
+++ src/components/notification/no-notification.tsx
@@ -3,7 +3,7 @@ import { IconBell } from '@tabler/icons-react'
 export const NoNotification = () => {
   return (
     <div className=' flex h-40 w-60 flex-col items-center justify-center gap-y-3'>
-      <span className='hover:cursor-default'>まだ通知はありません</span>
+      <span className='hover:cursor-default dark:brightness-75'>まだ通知はありません</span>
       <IconBell
         size={50}
         stroke={2}
