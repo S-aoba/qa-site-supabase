7ba47c9f7df7b9631a4b2b2aa6dd1413f2b4a77d
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-08-14 23:12:02 +0900

    🔥 rm:background-color

--- src/components/ui/error-message.tsx
+++ src/components/ui/error-message.tsx
@@ -1,6 +1,6 @@
 export const ErrorMessage = ({ message }: { message: string }) => {
   return (
-    <div className='mt-5 break-words bg-red-50 text-start text-sm font-semibold leading-loose tracking-wide text-red-500'>
+    <div className='mt-5 break-words text-start text-sm font-semibold leading-loose tracking-wide text-red-500'>
       {message}
     </div>
   )
