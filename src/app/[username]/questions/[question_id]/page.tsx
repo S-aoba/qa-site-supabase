a639bb8592822d32ac316fb85fd704eea37fb333
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-08-15 17:43:32 +0900

    🩹  fix: meta data

--- src/app/[username]/questions/[question_id]/page.tsx
+++ src/app/[username]/questions/[question_id]/page.tsx
@@ -1,8 +1,26 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import type { Metadata } from 'next'
 import { cookies } from 'next/headers'
 
 import { Question } from '@/components/question/question'
 import type { Database } from '@/lib/database.types'
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { username: string; question_id: string }
+}): Promise<Metadata> => {
+  // read route params
+  const question_id = params.question_id
+  const supabase = createServerComponentClient<Database>({
+    cookies,
+  })
+  const { data: question } = await supabase.from('questions').select('*, profiles(*)').eq('id', question_id).single()
+
+  return {
+    title: `${question?.title} - QA-site-supabase` ,
+  }
+}
 
 const QuestionPage = async ({ params }: { params: { username: string; question_id: string } }) => {
   const supabase = createServerComponentClient<Database>({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import { NewQuestionList } from '@/components/new-question-list/new-question-list'
 
+export const metadata = {
+  title: '新着質問一覧 - QA-site-supabase',
+  description: '新着質問一覧 - QA-site-supabase',
+}
+
 export default function Home() {
   return <NewQuestionList />
 }
