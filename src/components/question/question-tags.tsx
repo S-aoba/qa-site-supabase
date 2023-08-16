50f988e3461aa9bdbad80408bace0f4ef7c41b90
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-08-16 19:12:38 +0900

    👔  add: tagの先頭の文字を小文字にする関数の追加

--- src/components/question/question-tags.tsx
+++ src/components/question/question-tags.tsx
@@ -2,18 +2,29 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export const QuestionTags = ({ tags }: { tags: string[] }) => {
+  const capitalizeFirstLetter = (tag: string) => {
+    if (tag.length === 0) {
+      return tag // 空文字列の場合はそのまま返す
+    }
+
+    const firstLetter = tag.charAt(0).toLowerCase()
+    const restOfString = tag.slice(1)
+
+    return firstLetter + restOfString
+  }
   return (
-    <div className='flex space-x-3 overflow-x-hidden p-2 text-sm border-b border-input dark:brightness-75'>
+    <div className='flex space-x-3 overflow-x-hidden border-b border-input p-2 text-sm dark:brightness-75'>
       {tags.map((tag, index) => {
+        const modifiedTag = capitalizeFirstLetter(tag)
         return (
           <Link
             key={index}
             href={'/'}
-            className='flex items-center space-x-2 rounded-xl border px-2 py-1'
+            className='flex items-center space-x-2 rounded-xl border border-border px-2 py-1'
           >
             <div className='relative h-4 w-4'>
               <Image
-                src={`/lang-icon/${tag}.svg`}
+                src={`/lang-icon/${modifiedTag}.svg`}
                 className='rounded-full object-cover'
                 alt='avatar'
                 fill
