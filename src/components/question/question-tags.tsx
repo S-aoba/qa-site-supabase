6b7de76310b1e11860ca07660a3a16eef2b38026
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-08-16 22:53:02 +0900

    ♻️  ref: Link削除

--- src/components/question/question-tags.tsx
+++ src/components/question/question-tags.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
 import { capitalizeFirstLetter } from '@/common/function/convert'
 
@@ -9,11 +8,7 @@ export const QuestionTags = ({ tags }: { tags: string[] }) => {
       {tags.map((tag, index) => {
         const modifiedTag = capitalizeFirstLetter(tag)
         return (
-          <Link
-            key={index}
-            href={'/'}
-            className='flex items-center space-x-2 rounded-xl border border-border px-2 py-1'
-          >
+          <div key={index} className='flex items-center space-x-2 rounded-xl border border-border px-2 py-1'>
             <div className='relative h-4 w-4'>
               <Image
                 src={`/lang-icon/${modifiedTag}.svg`}
@@ -25,7 +20,7 @@ export const QuestionTags = ({ tags }: { tags: string[] }) => {
               />
             </div>
             <span className=''>{tag}</span>
-          </Link>
+          </div>
         )
       })}
     </div>
